67a1ce85ab298daa2c07a78a1cb9eeeb946680f2
mounirammari312 <mounirammari550@gmail.com>
2026-09-12 07:20:57 +0100

    Update Spinner.jsx

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Spinner.module.css";
 
 const Spinner = () => {
   return (
-    <div class={styles.ldsRoller}>
+    <div className={styles.ldsRoller}>
       <div></div>
       <div></div>
       <div></div>
